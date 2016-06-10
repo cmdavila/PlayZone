4149fd3ad5f25ecf8613f20b01ba716dc1df6109
Pastricio <pdespinosa@udlanet.ec>
2016-06-09 23:10:23 -0500

    cambio PlayZoneCliente

--- src/paqueticos/PlayZoneCliente.java
+++ src/paqueticos/PlayZoneCliente.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,21 +15,3 @@ public class PlayZoneCliente {
     
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package paqueticos;
-
-/**
- *
- * @author PATRICIO
- */
-public class PlayZoneCliente {
-    
-    
-    
-}
->>>>>>> 618b5ea6c9d5a963600d8bbe5cae01a2700e0bf9
